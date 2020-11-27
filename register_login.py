2bcd8081b0599483bd269c0250ac1fad1fcee04e
Kwon-subin <62585859+Kwon-subin@users.noreply.github.com>
2020-11-27 23:22:53 +0900

    로그인 및 회원가입 v.3.1
    세션 추가 및 간단한 오타 수정

--- register_login.py
+++ register_login.py
@@ -44,7 +44,7 @@ def register_student():
         y_str = request.form['year']
         m_str = request.form['month']
         d_str = request.form['day']
-        birth_str = y_str + m_str + d_str
+        birth_str = y_str + '-' + m_str + "-" + d_str
         n = int(y_str)
         today_y = datetime.today().year
         today_m = datetime.today().month
@@ -59,13 +59,16 @@ def register_student():
                 age = age+1
 
         #accountDic['age'] = age
-
+        f_phone = request.form['f_phone']
+        s_phone = request.form['s_phone']
+        t_phone = request.form['t_phone']
+        phone = f_phone + '-' + s_phone + '-' + t_phone
         accountDic = {
             'PW' : request.form['passwd'],
             'name' : request.form['name'],
             'age' : age, #만나이로 통일
             'birth' : birth_str, #YYYYMMDD
-            'phone' :  request.form['phone'], #'-'포함
+            'phone' :  phone, #'-'포함
             'T' : False #Teacher, Teacher > True, Student > False
         }
         res = es.get_doc('account', id)
@@ -83,57 +86,55 @@ def register_student():
 @app.route('/register/teacher', methods=['POST'])
 def register_teacher():
     if request.method == "POST":
-        if request.method == 'POST':
-            T_data = True
-            id = request.form['member_id']
-            y_str = request.form['year']
-            m_str = request.form['month']
-            d_str = request.form['day']
-            birth_str = y_str + m_str + d_str
-            n = int(y_str)
-            today_y = datetime.today().year
-            today_m = datetime.today().month
-            today_d = datetime.today().day
-            age = today_y - n
-            return_m = today_m - int(m_str)
-            if return_m > 0:
-                age = age + 1
-            if return_m == 0:
-                return_d = today_d - int(d_str)
-                if return_d >= 0:
-                    age = age+1
-
+        T_data = True
+        id = request.form['member_id']
+        y_str = request.form['year']
+        m_str = request.form['month']
+        d_str = request.form['day']
+        birth_str = y_str + '-' + m_str + "-" + d_str
+        n = int(y_str)
+        today_y = datetime.today().year
+        today_m = datetime.today().month
+        today_d = datetime.today().day
+        age = today_y - n
+        return_m = today_m - int(m_str)
+        if return_m > 0:
+            age = age + 1
+        if return_m == 0:
+            return_d = today_d - int(d_str)
+            if return_d >= 0:
+                age = age+1
             #accountDic['age'] = age
-
-            accountDic = {
-                'PW' : request.form['passwd'],
-                'name' : request.form['name'],
-                'age' : age, #만나이로 통일
-                'birth' : birth_str, #YYYYMMDD
-                'phone' :  request.form['phone'], #'-'포함
-                'T' : True #Teacher, Teacher > True, Student > False
-            }
-
-            cer = request.form['certificate_num']
-            cer_num = int(cer)
-            place = request.form['place']
-
-            teacher = {
-                'certificate_num' : cer_num,
-                'place' : place #OOO도 OO시 / OOOO시
-            }
-
-            res = es.get_doc('account', id)
-            if res == -1:
-                res2 = es.insert_doc('account' ,id, accountDic)
-                res3 = es.insert_doc('teacher', id, teacher)
-                if res2 == 0 and res3 == 0:
-                    return render_template('register_s.html')
-                else: #디버깅
-                    return render_template('register_f.html')
-                    #return render_template('register_page.html')
-            else: #있는 아이디 인 경우
-                return render_template('register_choose.html')
+        f_phone = request.form['f_phone']
+        s_phone = request.form['s_phone']
+        t_phone = request.form['t_phone']
+        phone = f_phone + '-' + s_phone + '-' + t_phone
+        accountDic = {
+            'PW' : request.form['passwd'],
+            'name' : request.form['name'],
+            'age' : age, #만나이로 통일
+            'birth' : birth_str, #YYYYMMDD
+            'phone' :  phone, #'-'포함
+            'T' : True #Teacher, Teacher > True, Student > False
+        }
+        cer = request.form['certificate_num']
+        cer_num = int(cer)
+        place = request.form['place']
+        teacher = {
+            'certificate_num' : cer_num,
+            'place' : place #OOO도 OO시 / OOOO시
+        }
+        res = es.get_doc('account', id)
+        if res == -1:
+            res2 = es.insert_doc('account' ,id, accountDic)
+            res3 = es.insert_doc('teacher', id, teacher)
+            if res2 == 0 and res3 == 0:
+                return render_template('register_s.html')
+            else: #디버깅
+                return render_template('register_f.html')
+                #return render_template('register_page.html')
+        else: #있는 아이디 인 경우
+            return render_template('register_choose.html')
 
 @app.route('/login_input', methods=['POST'])
 def login():
@@ -145,9 +146,10 @@ def login():
             return render_template('login.html')
         else:
             if res['_source']['PW'] == user_password:
-            #session['user_id'] = id
-            #session['T'] = res['T']
-                return render_template('register_s.html')
+                session['user_id'] = user_id
+                session['name'] = res['_source']['name']
+                session['T'] = res['_source']['T']
+                return render_template('login_s.html')
             else:
                 return render_template('login.html')
 
