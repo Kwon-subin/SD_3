350ec7576ef72d8c89b96c1f67288b8b038171e2
kcsol <62585142+kcsol@users.noreply.github.com>
2020-12-16 23:29:16 +0900

    카테고리 v.final
    카테고리 py files

--- main_category.py
+++ main_category.py
@@ -1,7 +1,26 @@
+import ESGuide as es
 from flask import Blueprint, render_template, request, session
 
 
 bp_class = Blueprint('class', __name__)
+
+
+def getClasses(ctgry_name, ctgry_detail):
+        ctgry = es.get_doc('category', ctgry_name)
+        print(ctgry)
+        class_ids_ = ctgry['_source']['detail']
+        class_ids = None
+        for class_ids in class_ids_:
+            if class_ids['name'] == ctgry_detail:
+                break
+        class_ids = class_ids['class']
+
+        if len(class_ids) == 0:
+            return []
+        
+        classes = es.get_docs('class', class_ids)
+
+        return classes
 
 
 @bp_class.route('/main_class')
@@ -25,73 +44,109 @@ def experience():
     return render_template("class_category_experience.html")
 
 
+
+
 @bp_class.route('/experience/craft')
 def experience_craft():
-    return render_template("class_show.html",data = '/experience/craft', teacher = session['T'],
-        b_category = 'experience', s_category='craft')
+    classes = getClasses('experience', 'craft')
+    return render_template("class_list.html",data = '/experience/craft', teacher = session['T'],
+        b_category = 'experience', s_category='craft', reading = classes)
+
 @bp_class.route('/experience/dance_vocal')
 def experience_dance_vocal():
-    return render_template("class_show.html",data= '/experience/dance_vocal', teacher = session['T'],
-        b_category = 'experience', s_category='dance_vocal')
+    classes = getClasses('experience', 'dance_vocal')
+    return render_template("class_list.html",data= '/experience/dance_vocal', teacher = session['T'],
+        b_category = 'experience', s_category='dance_vocal', reading = classes)
+
 @bp_class.route('/experience/flower')
 def experience_flower():
-    return render_template("class_show.html",data='/experience/flower', teacher = session['T'],
-        b_category = 'experience', s_category='flower')
+    classes = getClasses('experience', 'flower')
+    return render_template("class_list.html",data='/experience/flower', teacher = session['T'],
+        b_category = 'experience', s_category='flower', reading = classes)
+
 @bp_class.route('/experience/etc')
 def experience_etc():
-    return render_template("class_show.html",data='/experience/etc', teacher = session['T'],
-        b_category = 'experience', s_category='etc')
+    classes = getClasses('experience', 'etc')
+    return render_template("class_list.html",data='/experience/etc', teacher = session['T'],
+        b_category = 'experience', s_category='etc', reading = classes)
+
+
 
 
 @bp_class.route('/beauty/cosmetic')
 def beauty_cosmetic():
-    return render_template("class_show.html",data='/beauty/cosmetic', teacher = session['T'],
-        b_category = 'beauty', s_category='cosmetic')
+    classes = getClasses('beauty', 'cosmetic')
+    return render_template("class_list.html",data='/beauty/cosmetic', teacher = session['T'],
+        b_category = 'beauty', s_category='cosmetic', reading = classes)
+
 @bp_class.route('/beauty/soap')
 def beauty_soap():
-    return render_template("class_show.html",data='/beauty/soap', teacher = session['T'],
-        b_category = 'beauty', s_category='soap')
+    classes = getClasses('beauty', 'soap')
+    return render_template("class_list.html",data='/beauty/soap', teacher = session['T'],
+        b_category = 'beauty', s_category='soap', reading = classes)
+        
 @bp_class.route('/beauty/perfume')
 def beauty_perfume():
-    return render_template("class_show.html",data='/beauty/perfume', teacher = session['T'],
-        b_category = 'beauty', s_category='perfume')
+    classes = getClasses('beauty', 'perfume')
+    return render_template("class_list.html",data='/beauty/perfume', teacher = session['T'],
+        b_category = 'beauty', s_category='perfume', reading = classes)
+
 @bp_class.route('/beauty/etc')
 def beauty_etc():
-    return render_template("class_show.html",data='/beauty/etc', teacher = session['T'],
-        b_category = 'beauty', s_category='etc')
+    classes = getClasses('beauty', 'etc')
+    return render_template("class_list.html",data='/beauty/etc', teacher = session['T'],
+        b_category = 'beauty', s_category='etc', reading = classes)
+
+
 
 
 @bp_class.route('/cooking/baking')
 def cooking_baking():
-    return render_template("class_show.html",data='/cooking/baking', teacher = session['T'],
-        b_category = 'cooking', s_category='baking')
+    classes = getClasses('cooking', 'baking')
+    return render_template("class_list.html",data='/cooking/baking', teacher = session['T'],
+        b_category = 'cooking', s_category='baking', reading = classes)
+
 @bp_class.route('/cooking/drink')
 def cooking_drink():
-    return render_template("class_show.html",data='/cooking/drink', teacher = session['T'],
-        b_category = 'cooking', s_category='drink')
+    classes = getClasses('cooking', 'drink')
+    return render_template("class_list.html",data='/cooking/drink', teacher = session['T'],
+        b_category = 'cooking', s_category='drink', reading = classes)
+
 @bp_class.route('/cooking/meal')
 def cooking_meal():
-    return render_template("class_show.html",data='/cooking/meal', teacher = session['T'],
-        b_category = 'cooking', s_category='meal')
+    classes = getClasses('cooking', 'meal')
+    return render_template("class_list.html",data='/cooking/meal', teacher = session['T'],
+        b_category = 'cooking', s_category='meal', reading = classes)
+
 @bp_class.route('/cooking/etc')
 def cooking_etc():
-    return render_template("class_show.html",data='/cooking/etc', teacher = session['T'],
-        b_category = 'cooking', s_category='etc')
+    classes = getClasses('cooking', 'etc')
+    return render_template("class_list.html",data='/cooking/etc', teacher = session['T'],
+        b_category = 'cooking', s_category='etc', reading = classes)
+
+
 
 
 @bp_class.route('/art/handwriting')
 def art_handwriting():
-    return render_template("class_show.html",data='/art/handwriting', teacher = session['T'],
-        b_category = 'art', s_category='handwriting')
+    classes = getClasses('art', 'handwriting')
+    return render_template("class_list.html",data='/art/handwriting', teacher = session['T'],
+        b_category = 'art', s_category='handwriting', reading = classes)
+
 @bp_class.route('/art/drawing')
 def art_drawing():
-    return render_template("class_show.html",data='/art/drawing', teacher = session['T'],
-        b_category = 'art', s_category='drawing')
+    classes = getClasses('art', 'drawing')
+    return render_template("class_list.html",data='/art/drawing', teacher = session['T'],
+        b_category = 'art', s_category='drawing', reading = classes)
+
 @bp_class.route('/art/coloring')
 def art_coloring():
-    return render_template("class_show.html",data='/art/coloring', teacher = session['T'],
-        b_category = 'art', s_category='coloring')
+    classes = getClasses('art', 'coloring')
+    return render_template("class_list.html",data='/art/coloring', teacher = session['T'],
+        b_category = 'art', s_category='coloring', reading = classes)
+
 @bp_class.route('/art/etc')
 def art_etc():
-    return render_template("class_show.html",data='/art/etc', teacher = session['T'],
-        b_category = 'art', s_category='etc')
+    classes = getClasses('art', 'etc')
+    return render_template("class_list.html",data='/art/etc', teacher = session['T'],
+        b_category = 'art', s_category='etc', reading = classes)
--- makeClass.py
+++ makeClass.py
@@ -15,6 +15,26 @@ from elasticsearch import helpers
 bp_makeClass = Blueprint('makeClass', __name__)
 bp_makeClass.secret_key = "SD_3"
 
+CLASS_NUM = "0000"
+
+
+def get_cn():
+    global CLASS_NUM
+
+    doc = es.get_doc('class_num', 'class_num')
+    if doc == -1:
+        CLASS_NUM = "{:>04d}".format(0)
+    else:
+        CLASS_NUM = doc['_source']['CLASS_NUM']
+
+    return CLASS_NUM
+
+
+def update_cn():
+    es.insert_doc('class_num', 'class_num', {'CLASS_NUM' : CLASS_NUM})
+
+
+
 @bp_makeClass.route('/')
 def main():
     #if session['T'] == "True":
@@ -36,22 +56,25 @@ def makeClass():
         m_str = str(datetime.today().month) #현재 월
         d_str = str(datetime.today().day) #현재 일
         h_str = str(datetime.now().hour) #현재 시
-        m_str = str(datetime.now().minute) #현재 월
+        min_str = str(datetime.now().minute) #현재 월
         cost_str = request.form['cost']
         c_name = request.form['c_name']
         cat_name = request.form['cat_name']
         cat_detail = request.form['cat_detail']
         c_content = request.form['content']
-        m_str = str(request.form.get('m'))
-        when = y_str + '-' + m_str + '-' + d_str + '/' + h_str + ':' + m_str
+        meet_str = str(request.form.get('m'))
+        when = y_str + '-' + m_str + '-' + d_str + '/' + h_str + ':' + min_str
         start = request.form['start']
         end = request.form['end']
         cost = int(cost_str)
         ID = session['user_id']
-        pid = ID + y_str + m_str + d_str + h_str + m_str #예 : tn125620201127
-        if m_str == "offline":
+
+        global CLASS_NUM
+        get_cn()
+    
+        if meet_str == "offline":
             M = True
-        if m_str == "online":
+        if meet_str == "online":
             M = False
 
         i_class = {
@@ -59,15 +82,22 @@ def makeClass():
             'c_name' : c_name,
             'cost' : cost,
             'start' : start, #YYYY-MM-DD/
-            'end' : end
+            'end' : end,
             'M' : M, #Meet, Offline > True, Online > False
-            'c_content' : content,
+            'c_content' : c_content,
             #'cat_name' : cat_name,
             #'cat_detail' : cat_detail,
         }
-        res = es.insert_doc('class',pid,i_class)
-        es.addToCategory(cat_name, cat_detail, 'class', pid)
-        return redirect(url_for('class.'+cat_name+'_'+cat_detail))
+        res = es.insert_doc('class', CLASS_NUM,i_class)
+        es.addToCategory(cat_name, cat_detail, 'class', CLASS_NUM)
+        cn = int(CLASS_NUM) + 1
+        if(cn > 9999):
+            return -1
+        CLASS_NUM = "{:>04d}".format(cn)
+        update_cn()
+
+
+        return render_template('confirmClass.html', b_category=cat_name, s_category=cat_detail, _class=i_class)
 
 if __name__ == '__main__':
     bp_makeClass.run(host='127.0.0.1', port=5000, debug=True)
